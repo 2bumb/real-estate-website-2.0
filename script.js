f91b969def98fd41a72a68816fffc3c469bb82c5
2bumb <alee62580@gmail.com>
2024-02-15 21:49:22 -0800

    added

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstname = "Steve";
+let lastname = "Jobs";
+let gains=true;
+document.getElementById("p1").textContent = Firstname;
+document.getElementById("p2").textContent = Lastname;
+
+
+
