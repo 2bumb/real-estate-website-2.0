df33f6729c4eff518a624ab13f4a8cb29ca07fa3
2bumb <alee62580@gmail.com>
2024-02-16 20:28:14 -0800

    changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let firstname = "Steve";
-let lastname = "Jobs";
-let gains=true;
-document.getElementById("p1").textContent = Firstname;
-document.getElementById("p2").textContent = Lastname;
+// let firstname = "Steve";
+// let lastname = "Jobs";
+// let gains=true;
+// document.getElementById("p1").textContent = Firstname;
+// document.getElementById("p2").textContent = Lastname;
 
 
 
