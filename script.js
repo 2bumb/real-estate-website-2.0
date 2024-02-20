3a2588967d2969cfa22e415a1d7a6a2ea70b7cf8
2bumb <alee62580@gmail.com>
2024-02-19 20:08:47 -0800

    s

--- script.js
+++ script.js
@@ -8,3 +8,14 @@
 
 
 
+let age = 6; // Example age value
+
+if (age > 20) {
+    document.write("I am over 20");
+} else if (age > 10) {
+    document.write("I am over 10");
+}else if (age > 5) {
+    document.write("I am over 5");
+}else {
+    document.write("I am under 7");
+}
