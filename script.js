39477d846ada3f7b76a271d0e30af4ef3e8cdc2d
2bumb <alee62580@gmail.com>
2024-02-20 18:59:56 -0800

    added changes

--- script.js
+++ script.js
@@ -8,14 +8,31 @@
 
 
 
-let age = 6; // Example age value
+// let age = 6; // Example age value
 
-if (age > 20) {
-    document.write("I am over 20");
-} else if (age > 10) {
-    document.write("I am over 10");
-}else if (age > 5) {
-    document.write("I am over 5");
-}else {
-    document.write("I am under 7");
+// if (age > 20) {
+//     document.write("I am over 20");
+// } else if (age > 10) {
+//     document.write("I am over 10");
+// }else if (age > 5) {
+//     document.write("I am over 5");
+// }else {
+//     document.write("I am under 7");
+// }
+
+
+var myAge = 24;
+
+if (myAge < 18 || myAge > 30) { // we are saying my age 24 must be under 18 years old and the other one is saying 24 is less than 30
+    document.write("you cool bro");
+} else {
+    document.write("you aint cool");
 }
+
+// let myAge = 35;
+
+// if (myAge < 18|| myAge > 30){ // Syntax issue here
+//     document.write("you cool bro");
+// } else {
+//     document.write("you aint cool");
+// }
